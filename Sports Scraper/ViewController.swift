827a723c0fa2771607b5a801ffc4ed296929554b
Kevin Escobedo <escobedo001@gmail.com>
2020-08-06 17:03:48 -0700

    Initial Commit

---
+++ Sports Scraper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sports Scraper
+//
+//  Created by Kevin Escobedo on 8/6/20.
+//  Copyright © 2020 Kevin Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
